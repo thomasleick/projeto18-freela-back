5edd0f10a1ee4c0f374325ef9f27cf60d9ff00ca
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-05-27 01:23:44 -0300

    fix: getFlightsPage query w/ join to return names

--- src/services/flightsService.js
+++ src/services/flightsService.js
@@ -1,9 +1,9 @@
 import pool from "../configs/dbConn.js";
 
 export const getFlightsCount = async (filters) => {
-  let query = "SELECT COUNT(*) AS total_rows FROM flights WHERE 1=1";
+  let query = "SELECT COUNT(f.*) AS total_rows FROM flights f WHERE 1=1";
 
-  const [kiwi, values] = buildKiwi({ ...filters, order:"" });
+  const [kiwi, values] = buildKiwi({ ...filters, order: "" });
   query += kiwi;
   const client = await pool.connect();
   try {
@@ -25,7 +25,15 @@ export const getFlightsPage = async (filters) => {
   const { page, limit } = filters;
   const parsedPage = page || 1;
   const offset = limit * (parsedPage - 1);
-  let query = "SELECT * FROM flights WHERE 1=1";
+  let query = 
+    `SELECT f.flight_id, a.airline_name, dc.city_name AS departure_city_name, dd.city_name AS 
+        destination_city_name, f.departure_time, f.arrival_time, f.price 
+    FROM flights f
+    JOIN airlines a ON f.airline_id = a.airline_id
+    JOIN cities dc ON f.departure_city_id = dc.city_id
+    JOIN cities dd ON f.destination_city_id = dd.city_id
+    WHERE 1=1`;
+
   const [kiwi, values] = buildKiwi({ ...filters, offset });
   query += kiwi;
   const client = await pool.connect();
@@ -34,7 +42,7 @@ export const getFlightsPage = async (filters) => {
       text: query,
       values: values,
     };
-    console.log(fullQuery)
+    console.log(fullQuery);
     const count = await client.query(fullQuery);
     return count.rows;
   } catch (err) {
@@ -65,55 +73,55 @@ const buildKiwi = (filters) => {
   let query = ``;
 
   if (airline_id) {
-    query += ` AND airline_id = $${i}`;
+    query += ` AND f.airline_id = $${i}`;
     values.push(airline_id);
     i++;
   }
   if (departure_city_id) {
-    query += ` AND departure_city_id = $${i}`;
+    query += ` AND f.departure_city_id = $${i}`;
     values.push(departure_city_id);
     i++;
   }
   if (destination_city_id) {
-    query += ` AND destination_city_id = $${i}`;
+    query += ` AND f.destination_city_id = $${i}`;
     values.push(destination_city_id);
     i++;
   }
   if (departure_time) {
-    query += ` AND departure_time = $${i}`;
+    query += ` AND f.departure_time = $${i}`;
     values.push(departure_time);
     i++;
   }
   if (arrival_time) {
-    query += ` AND arrival_time = $${i}`;
+    query += ` AND f.arrival_time = $${i}`;
     values.push(arrival_time);
     i++;
   }
   if (minPrice) {
-    query += ` AND price >= $${i}`;
+    query += ` AND f.price >= $${i}`;
     values.push(minPrice);
     i++;
   }
   if (maxPrice) {
-    query += ` AND price <= $${i}`;
+    query += ` AND f.price <= $${i}`;
     values.push(maxPrice);
     i++;
   }
   if (order) {
-    query += ` ORDER BY ${order}`;
-    if(desc){
-        query += ` DESC`;
+    query += ` ORDER BY f.${order}`;
+    if (desc) {
+      query += ` DESC`;
     }
   }
   if (limit) {
-    query += ` LIMIT $${i}`
-    values.push(limit)
-    i++
+    query += ` LIMIT $${i}`;
+    values.push(limit);
+    i++;
   }
   if (offset) {
-    query += ` OFFSET $${i}`
-    values.push(offset)
-    i++
+    query += ` OFFSET $${i}`;
+    values.push(offset);
+    i++;
   }
   return [query, values];
 };
