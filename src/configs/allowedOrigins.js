12bbb4dc132e778cd4407a527eb807d6327df591
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-05-29 11:52:23 -0300

    fix: changing allowedOrigins to match vercel

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -4,12 +4,8 @@ const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://projeto17-shortly-front.vercel.app/",
-  "http://projeto17-shortly-front.vercel.app/",
-  "projeto17-shortly-front.vercel.app/",
-  "https://projeto17-shortly-front.vercel.app",
-  "http://projeto17-shortly-front.vercel.app",
-  "projeto17-shortly-front.vercel.app",
+  "https://projeto18-freela-front-theta.vercel.app/",
+  "https://projeto18-freela-front-theta.vercel.app",
 ];
 
 export default allowedOrigins;
